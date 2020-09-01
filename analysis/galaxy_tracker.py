1fd44e3559d3803532369955cc9ca142c38b5428
Jonah Rose <j.rose@ufl.edu>
2020-09-01 19:40:40 -0400

    Began analysis folder

---
+++ analysis/galaxy_tracker.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import readData.loadData.py as load
+
+def track_largest():
+    return
---
+++ analysis/make_image.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import RoseArepo.loadData as load
+
+def hist():
+    return
