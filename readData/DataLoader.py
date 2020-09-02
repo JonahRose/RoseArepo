2e76314faf787d61dd26405f381353e14858861a
Jonah Rose <j.rose@ufl.edu>
2020-09-02 14:41:22 -0400

    Added part_types

--- readData/DataLoader.py
+++ readData/DataLoader.py
@@ -1,14 +1,16 @@
-import readData.load_data.py
+import readData.load_data as load
 import numpy as np
 
 
 class DataLoader():
 
-    def __init__(self, path, keys):
+    def __init__(self, path, part_types, keys):
         self.path = path
         self.input_keys = path
 
     def get_correct_keys(self):
+        for key in self.input_keys:
+            pass
         return
 
     def _load_data(self):
