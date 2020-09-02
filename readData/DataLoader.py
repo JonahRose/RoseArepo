fdf1e9601a3ff80abdc7fbf9f21aebfb6a18972a
Jonah Rose <j.rose@ufl.edu>
2020-09-02 14:05:20 -0400

    Started DataLoader class

---
+++ readData/DataLoader.py
@@ -0,0 +1,15 @@
+import readData.load_data.py
+import numpy as np
+
+
+class DataLoader():
+
+    def __init__(self, path, keys):
+        self.path = path
+        self.input_keys = path
+
+    def get_correct_keys(self):
+        return
+
+    def _load_data(self):
+        return
