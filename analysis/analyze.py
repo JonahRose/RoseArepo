e195e346106ba207cab06268ee4a5aa1f03b87db
JonahRose <j.rose@ufl.edu>
2022-02-17 18:11:29 -0500

    Made first density image work on Python3

--- analysis/analyze.py
+++ analysis/analyze.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from readData.DataLoader import DataLoader
+import ctypes
 
 #accounts for wrapping around length of box
 def calc_dist(loc1, loc2, boxlength):
@@ -37,6 +38,56 @@ def get_box_cut(part_pos, center, length):
     is_z_in_box = (part_pos[:, 2] < zpr) & (part_pos[:, 2] > zmr)
     
     return is_x_in_box & is_y_in_box & is_z_in_box
+
+def checklen(x):
+    return len(np.array(x,ndmin=1));
+def fcor(x):
+    return np.array(x,dtype='f',ndmin=1)
+def vfloat(x):
+    return x.ctypes.data_as(ctypes.POINTER(ctypes.c_float));
+
+def ok_scan(input,xmax=1.0e10,pos=0):
+    if (pos==1):
+        return (np.isnan(input)==False) & (abs(input)<=xmax) & (input > 0.);
+    else:
+        return (np.isnan(input)==False) & (abs(input)<=xmax);
+
+def int_round(x):
+    return np.int(np.round(x));
+
+def get_particle_hsml(x, y, z, DesNgb=32, Hmax=0):
+    x = fcor(x)
+    y = fcor(y)
+    z = fcor(z)
+    N = checklen(x)
+
+    ok = ok_scan(x) & ok_scan(y) & ok_scan(z)
+    x = x[ok]
+    y = y[ok]
+    z = z[ok]
+
+    if Hmax == 0:
+        dx = np.max(x) - np.min(x)
+        dy = np.max(y) - np.min(y)
+        dz = np.max(z) - np.min(z)
+        ddx = np.max([dx, dy, dz])
+        Hmax = 5*ddx*np.power(np.float(N), -1/3)
+
+    exec_call = '/home/j.rose/torreylabtools_git/C/c_libraries/' + '/StellarHsml/starhsml.so'
+    h_routine = ctypes.cdll[exec_call]
+
+    h_out_cast = ctypes.c_float*N
+    H_OUT = h_out_cast()
+
+    h_routine.stellarhsml(ctypes.c_int(N),
+                          vfloat(x), vfloat(y), vfloat(z),
+                          ctypes.c_int(DesNgb),
+                          ctypes.c_float(Hmax),
+                          ctypes.byref(H_OUT) )
+        
+    h = np.ctypeslib.as_array(H_OUT)
+    return h
+
 
 def calc_dens(path, snap, gal_pos, min_r=0.1, max_r=800, r_step=1.05):
 
--- visualizations/image_maker.py
+++ visualizations/image_maker.py
@@ -1,53 +1,176 @@
 import numpy as np
 import ctypes
 
+from readData.DataLoader import DataLoader
+from analysis import analyze
 
+class ImageData():
 
+    def __init__(self, cat):
 
-def fcor(x):
-    return np.array(x,dtype='f',ndmin=1)
-def vfloat(x):
-    return x.ctypes.data_as(ctypes.POINTER(ctypes.c_float));
-def cfloat(x):
-    return ctypes.c_float(x);
-def checklen(x):
-    return len(np.array(x,ndmin=1));
+        #load in values from DataLoader
+        self.pos = cat['Coordinates'] / cat.h * cat.time
+        self.vels = cat['Velocities'] * np.sqrt(cat.time) 
+        self.masses = cat['Masses'] * 1e10 / cat.h
 
+        #check if hsml is stored in the snapshot.
+        #if not, calculate it ourselves
+        if 'SubfindHsml' in cat:
+            self.hsml = cat['SubfindHsml'] / cat.h * cat.time
+        else:
+            self.hsml = analyze.get_particle_hsml(self.pos[:,0], self.pos[:,1], self.pos[:,2]) 
 
+        #check if this in an image of a single galaxy/group
+        if cat.sub_idx != -1:
+            self.gal_pos = cat['SubhaloPos'] / cat.h * cat.time
+        elif cat.fof_idx != -1:
+            self.gal_pos = cat['GroupPos'] / cat.h * cat.time
+        else:
+            self.gal_pos = None
 
-def make_surface_density_image(parttype=0, xpixels=256, fov=10.0, axisratio=1.0, ma=None, mi=None ):
+        #initialize fields to be filled in later
+        self.phi = None
+        self.theta = None
 
+        self.xr = None
+        self.yr = None
+        self.zr = None
+
+        self.dynrange = None
+        self.maxden = None
     
+        self.is_centered = False
 
+        return
+
+    #center data so that either the center of the halo is [0,0,0]
+    #or the center of the particles that have been loaded 
+    def center_data(self):
+        if self.gal_pos is not None:
+            self.pos -= self.gal_pos
+        else:
+            for i in range(3):
+                self.pos[:,i] -= (np.max(self.pos[:,i]) + np.min(self.pos[:,i])) / 2
+        self.is_centered = True
+        return
+
+    #not necessary so far, but if you want to obtain these you can 
+    def get_phi_theta(self, is_face=True, is_edge=False):
+        phi, theta = analyze.get_rotate_data(self.pos, self.vels, self.masses, edge_on=is_edge, face_on=is_face, get_pt=True)
+        self.phi = phi
+        self.theta = theta
+        return phi, theta
+
+    #This function will calculate phi, theta automatically if you want a face- or edge-on image
+    #otherwise you must supply the phi and theta you want 
+    def rotate_data(self, phi=0, theta=0, is_face=True, is_edge=False):
+        self.pos, self.vels = analyze.get_rotate_data(self.pos, self.vels, self.masses, phi, theta, is_edge, is_face)
+        return
+
+    #if not set explicetly, set implicetly as maximum 1d length in x, y, or z
+    def set_image_bounds(self, xrange=None, yrange=None, zrange=None):
+        if xrange is not None:
+            if yrange is None:
+                yrange = xrange
+            if zrange is None:
+                zrange= xrange
+        else:
+            x_stretch = np.max(self.pos[:,0]) - np.min(self.pos[:,0])
+            y_stretch = np.max(self.pos[:,1]) - np.min(self.pos[:,1])
+            x_mid     = 0.5*( np.max( self.pos[:,0]) + np.min(self.pos[:,0]) )
+            y_mid     = 0.5*( np.max( self.pos[:,1]) + np.min(self.pos[:,1]) )
+            z_mid     = 0.5*( np.max( self.pos[:,2]) + np.min(self.pos[:,2]) )
+
+            stretch = np.max( [x_stretch, y_stretch] )
+
+            xrange = [ x_mid - stretch/2.0 , x_mid + stretch/2.0 ]
+            yrange = [ y_mid - stretch/2.0 , y_mid + stretch/2.0 ]
+            zrange = [ z_mid - stretch/2.0 , z_mid + stretch/2.0 ]       
+
+        self.xr = xrange
+        self.yr = yrange
+        self.zr = zrange
+            
+        return
+
+    #If not set explicitly, assumed as min max values
+    def set_image_stretch(self, dynrange=None, maxden=None):
+        self.dynrange = dynrange
+        self.maxden = maxden
+        return
+
+    #cut out a box based on set_image_bounds
+    def clip_particles(self):
+        cut = ( self.pos[:,0] > self.xr[0]) & \
+              ( self.pos[:,0] < self.xr[1]) & \
+              ( self.pos[:,1] > self.yr[0]) & \
+              ( self.pos[:,1] < self.yr[1]) & \
+              ( self.pos[:,2] > self.zr[0]) & \
+              ( self.pos[:,2] < self.zr[1]) 
+
+        self.pos = self.pos[cut]
+        self.vels = self.vels[cut]
+        self.masses = self.masses[cut]
+        self.hsml = self.hsml[cut]
+
+        return 
+
+    def rescale_hsml(self, h_rescale_factor=1):
+        self.hsml *= h_rescale_factor
+        return
+
+    def fcor(self, x):
+        return np.array(x,dtype='f',ndmin=1)
+
+    def vfloat(self, x):
+        return x.ctypes.data_as(ctypes.POINTER(ctypes.c_float));
+
+    def cfloat(self, x):
+        return ctypes.c_float(x);
+
+    def checklen(self, x):
+        return len(np.array(x,ndmin=1));
+
+def make_surface_density_image(path, snap_num, parttype=0, xpixels=256, fov=10.0, axisratio=1.0, ma=None, mi=None, sub_idx=-1, fof_idx=-1):
+
+    print("... loading data ...")
+
+    keys = ['Coordinates', 'Velocities', 'Masses', 'SubhaloPos', 'GroupPos', 'SubfindHsml']
+    cat = DataLoader(path, snap_num, part_types=parttype, keys=keys, sub_idx=sub_idx, fof_idx=fof_idx)
+
+    data = ImageData(cat)
 
     ### To be re-written by Jonah using RoseArepo ###
-    data = rs.snapshot_data( snapshot, parttype, **kwargs ) # possible tags: cosmo, fof_num, sub_num
-    data.snapdir = snapdir
-    data.snapnum = snapnum
+    #data = rs.snapshot_data( snapshot, parttype, **kwargs ) # possible tags: cosmo, fof_num, sub_num
+    #data.snapdir = snapdir
+    #data.snapnum = snapnum
 
-    print "... centering data ..."
-    data = idm.center_data( data, **kwargs )
+    print("... centering data ...")
+    #data = idm.center_data( data, **kwargs )
+    data.center_data()
 
+    #phi, theta = util.determine_rotation_angle( data, **kwargs )
+    print("... rotating data ...")
+    #data = idm.rotate_data( data, phi, theta )
+    data.rotate_data()
 
-    phi, theta = util.determine_rotation_angle( data, **kwargs )
-    print "... rotating data ..."
-    data = idm.rotate_data( data, phi, theta )
+    print("... image bounds ...")
+    #data = ip.determine_image_bounds( data, **kwargs )
+    data.set_image_bounds()
 
-    print "... image bounds ..."
-    data = ip.determine_image_bounds( data, **kwargs )
+    print("... image stretch ...")
+    #data = ip.determine_image_stretch( data, **kwargs )
+    data.set_image_stretch()
 
-    print "... image stretch ..."
-    data = ip.determine_image_stretch( data, **kwargs )
+    print("... clipping ...")
+    #data = idm.clip_particles( data,  data.xr, data.yr, data.zr,  **kwargs )
+    data.clip_particles()
 
-    print "... clipping ..."
-    data = idm.clip_particles( data,  data.xr, data.yr, data.zr,  **kwargs )
-
-    print "... rescaling ..."
-    data = idm.rescale_hsml(   data, **kwargs)
+    print("... rescaling ...")
+    #data = idm.rescale_hsml(   data, **kwargs)
+    data.rescale_hsml()
 
     ### At this point, have data object with required partice data.
-
-
 
 
 #    massmap,image = \
@@ -58,13 +181,28 @@ def make_surface_density_image(parttype=0, xpixels=256, fov=10.0, axisratio=1.0,
 
 
   ## load the routine we need
-    exec_call=util.return_python_routines_cdir()+'/SmoothedProjPFH/allnsmooth.so'
+    #exec_call=util.return_python_routines_cdir()+'/SmoothedProjPFH/allnsmooth.so'
+    exec_call = '/home/j.rose/torreylabtools_git/C/c_libraries/SmoothedProjPFH/allnsmooth.so'
     smooth_routine=ctypes.cdll[exec_call];
     ## make sure values to be passed are in the right format
 
-   ### Jonah would need to edit, to ref data fields loaded above (e.g., weight = mass)
+    ### Jonah would need to edit, to ref data fields loaded above (e.g., weight = mass)
 
-    N=checklen(x); x=fcor(x); y=fcor(y); M1=fcor(weight); M2=fcor(weight2); M3=fcor(weight3); H=fcor(hsml)
+    #N=checklen(x); x=fcor(x); y=fcor(y); M1=fcor(weight); M2=fcor(weight2); M3=fcor(weight3); H=fcor(hsml)
+
+    N=data.checklen(data.pos[:,0]) #not used
+    
+    x=data.fcor(data.pos[:,0])
+    y=data.fcor(data.pos[:,1])
+    
+    weight = data.masses
+    weight2 = weight3 = None
+
+    M1=data.fcor(weight)
+    M2=data.fcor(weight2)
+    M3=data.fcor(weight3)
+    
+    H=data.fcor(data.hsml)
 
     # could be packaged into a sep class dealing with image properties
     xpixels=np.int(xpixels); ypixels=np.int(xpixels * axisratio)
@@ -73,9 +211,9 @@ def make_surface_density_image(parttype=0, xpixels=256, fov=10.0, axisratio=1.0,
 
     ## check for whether the optional extra weights are set
     NM=1;
-    if(checklen(M2)==checklen(M1)):
+    if(data.checklen(M2)==data.checklen(M1)):
         NM=2;
-        if(checklen(M3)==checklen(M1)):
+        if(data.checklen(M3)==data.checklen(M1)):
             NM=3;
         else:
             M3=np.copy(M1);
@@ -86,20 +224,20 @@ def make_surface_density_image(parttype=0, xpixels=256, fov=10.0, axisratio=1.0,
     XYpix=xpixels*ypixels; MAP=ctypes.c_float*XYpix; MAP1=MAP(); MAP2=MAP(); MAP3=MAP();
     ## main call to the imaging routine
     smooth_routine.project_and_smooth( \
-        ctypes.c_int(N), \			# number of particles
-        vfloat(x), vfloat(y), 			# x/y pos of particles
-        vfloat(H), \				# hsml of particle 
-        ctypes.c_int(NM), \			# Number of map dimensions
-        vfloat(M1), vfloat(M2), vfloat(M3), \	# The 3 possible map weights
-        cfloat(xmin), cfloat(xmax), 		# x min/max for the image
-	cfloat(ymin), cfloat(ymax), \		# y min/max for the image
-        ctypes.c_int(xpixels), ctypes.c_int(ypixels), \ # just the number of pixels in each direction
-        ctypes.byref(MAP1), ctypes.byref(MAP2), ctypes.byref(MAP3) );	# "empty" arrays to hold the output
+        ctypes.c_int(N), 			# number of particles
+        data.vfloat(x), data.vfloat(y), 			# x/y pos of particles
+        data.vfloat(H), 				# hsml of particle 
+        ctypes.c_int(NM), 			# Number of map dimensions
+        data.vfloat(M1), data.vfloat(M2), data.vfloat(M3), 	# The 3 possible map weights
+        data.cfloat(xmin), data.cfloat(xmax), 		# x min/max for the image
+        data.cfloat(ymin), data.cfloat(ymax), 		# y min/max for the image
+        ctypes.c_int(xpixels), ctypes.c_int(ypixels),  # just the number of pixels in each direction
+        ctypes.byref(MAP1), ctypes.byref(MAP2), ctypes.byref(MAP3) )	# "empty" arrays to hold the output
 
     ## now put the output arrays into a useful format
-    MassMap1=np.ctypeslib.as_array(MAP1).reshape([xpixels,ypixels]);
-    MassMap2=np.ctypeslib.as_array(MAP2).reshape([xpixels,ypixels]);
-    MassMap3=np.ctypeslib.as_array(MAP3).reshape([xpixels,ypixels]);
+    MassMap1=np.ctypeslib.as_array(MAP1).reshape([xpixels,ypixels])
+    MassMap2=np.ctypeslib.as_array(MAP2).reshape([xpixels,ypixels])
+    MassMap3=np.ctypeslib.as_array(MAP3).reshape([xpixels,ypixels])
 
     # Here, the MassMaps should contain actual mass maps with units of [M1] / ( [dimensions of pos]^2 )
     # For example, if mass is in units of 10^{10} M_solar and pos is in kpc:  10^{10} M_solar / kpc ^2
@@ -107,7 +245,7 @@ def make_surface_density_image(parttype=0, xpixels=256, fov=10.0, axisratio=1.0,
 
     # set boundaries and do some clipping
     MassMap = np.copy(MassMap1);
-    print "MassMap : max: ", np.max(MassMap), "   min: ", np.min(MassMap)
+    print("MassMap : max: ", np.max(MassMap), "   min: ", np.min(MassMap))
 
 ####### could put similar functionality back in later.
 #    if (set_percent_maxden !=0) or (set_percent_minden !=0):
@@ -127,12 +265,14 @@ def make_surface_density_image(parttype=0, xpixels=256, fov=10.0, axisratio=1.0,
 #        if (ma <= 0) or (np.isnan(ma)): ma=np.max(Msort[ok]);
 #############
 
-    if(ma is None):  ma = np.max( MassMap )
-    if(mi is None):  mi = np.min( MassMap )
+    if ma is None:  
+        ma = np.max( MassMap )
+    if mi is None:  
+        mi = np.min( MassMap )
 
-    print "Clipping the weighted maps at   ma= ", ma, " mi= ", mi
-    MassMap[MassMap < mi]=mi; MassMap[MassMap > ma]=ma;
-
+    print("Clipping the weighted maps at   ma= ", ma, " mi= ", mi)
+    MassMap[MassMap < mi]=mi
+    MassMap[MassMap > ma]=ma
 
     # now set colors
     cols=255. # number of colors
